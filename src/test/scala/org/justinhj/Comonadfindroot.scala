3b7596c5ab6f04b3661056ad8dec181cd372f835
Justin Heyes-Jones <justinhj@gmail.com>
2022-04-08 23:57:56 -0700

    updates

--- src/test/scala/org/justinhj/Comonadfindroot.scala
+++ src/test/scala/org/justinhj/Comonadfindroot.scala
@@ -2,9 +2,14 @@ package org.justinhj
 
 import org.scalatest.flatspec.AnyFlatSpec
 
+import org.justinhj.ComonadFindRoot
+
 class Test1 extends AnyFlatSpec {
 
   "Test1" should "example test should pass" in {
+
+    
+
 
     assert(1+1 === 2)
 
